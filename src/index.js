483e61e986e88e747bb13d33763105dcd83cd244
Dmytro Tkachuk <tkachukdimon@gmail.com>
2020-07-08 20:52:08 +0300

    details branch at first added files: data.js,details.js,details.scss,details.hbs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import "./styles/styles.scss";
 import FetchApi from "./js/fetchApi.js";
-
+//подключает липовый объект
+import data from './js/data'
+//подключает страничку одного фильма
+import buildDetails from './js/details'
 const search = new FetchApi();
 search.getPopular(10).then((popularMovies) => console.log(popularMovies));
 const searchMovie = search.getByName("nemo");
@@ -9,3 +12,5 @@ searchMovie.then((m) => console.log(m));
 const viewer = document.querySelector("#postersViewer");
 
 console.log(viewer);
+//строит страничку одного фильма
+buildDetails(data)
