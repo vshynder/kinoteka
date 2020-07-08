3514ce755f370b1aa8dca00810c35508b64167f9
Vladyslav_Shynder <shynderv@gmail.com>
2020-07-08 22:05:27 +0300

    revived_header

--- src/js/details.js
+++ src/js/details.js
@@ -1,20 +1,14 @@
-import detailsTemplate from '../templates/details.hbs'
-const viewer = document.querySelector('#postersViewer');
+import detailsTemplate from "../templates/details.hbs";
+const viewer = document.querySelector("#postersViewer");
 
-export default function buildDetails(data){
-    
-    const markup = data.map(film => detailsTemplate(film)).join('')
-    // console.log(markup)
-    viewer.insertAdjacentHTML('afterbegin' , markup)
+export default function buildDetails(data) {
+  const markup = data.map((film) => detailsTemplate(film)).join("");
+  // console.log(markup)
+  viewer.insertAdjacentHTML("afterbegin", markup);
 
-    
-    // add to watched
-    const watchedBtn = viewer.querySelector('#watched')
-    console.log(watchedBtn)
-    // watchedBtn.addEventListener('change', console.log('yess'))
-    //add to queue
-    const queueBtn = viewer.querySelector('#queue')
-    console.log(queueBtn)
-
+  // add to watched
+  const watchedBtn = viewer.querySelector("#watched");
+  // watchedBtn.addEventListener('change', console.log('yess'))
+  //add to queue
+  const queueBtn = viewer.querySelector("#queue");
 }
-
