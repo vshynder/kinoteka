66a8c5853c2384b2b2815c2e7653a13db6666f25
Vladyslav_Shynder <shynderv@gmail.com>
2020-07-08 19:44:29 +0300

    fetch + fonts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import './styles/styles.scss';
+import "./styles/styles.scss";
+import FetchApi from "./js/fetchApi.js";
 
-const viewer = document.querySelector('#postersViewer');
+const search = new FetchApi();
+search.getPopular(10).then((popularMovies) => console.log(popularMovies));
+const searchMovie = search.getByName("nemo");
+searchMovie.then((m) => console.log(m));
+
+const viewer = document.querySelector("#postersViewer");
 
 console.log(viewer);
