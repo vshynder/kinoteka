e7bd2036d51a5df9332a1d822cb147643cafe14c
Vladyslav_Shynder <shynderv@gmail.com>
2020-07-08 21:18:08 +0300

    dev commit

--- src/index.js
+++ src/index.js
@@ -9,3 +9,10 @@ searchMovie.then((m) => console.log(m));
 const viewer = document.querySelector("#postersViewer");
 
 console.log(viewer);
+
+const GENRES = [];
+const genrseFetch =
+  "https://api.themoviedb.org/3/genre/movie/list?api_key=be244f548a27d8c36fc000d6ba379c12&language=en-US";
+fetch(genrseFetch)
+  .then((j) => j.json())
+  .then((d) => console.log(d));
