71918e0ff5eb300a15c679df215a1098db71629e
Евгения <macbook@MacBook-Air.local>
2020-07-11 00:53:58 +0300

    input-01

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import "./styles/styles.scss";
 import FetchApi from "./js/fetchApi.js";
 import { mainPageSlider } from "./js/details.js";
+import "./js/input.js";
 
 mainPageSlider.set(20);
+
+
 
 const search = new FetchApi();
 const searchMovie = search.getByName("nemo"); // <<<------сюда запихнуть переменную с строкой с инпута вместо "nemo"
@@ -14,3 +17,6 @@ const genrseFetch =
 fetch(genrseFetch)
   .then((j) => j.json())
   .then((d) => console.log(d));
+
+
+  // input();
