b7a926fa6abcf3c161982794822d58317270a9dd
Евгения <macbook@MacBook-Air.local>
2020-07-12 13:09:10 +0300

    header-02

--- src/js/input.js
+++ src/js/input.js
@@ -1,11 +1,24 @@
 import FetchAPI from "./FetchApi";
 
 const search = new FetchAPI();
+const errorString = document.querySelector('.js-error-notification')
+const homeButton = document.querySelector('.js-home-btn');
+const libraryButton = document.querySelector('.js-library-btn');
+
+
+
+homeButton.addEventListener('click',() => {});
+libraryButton.addEventListener('click',() => {});
+
 
 function eventMaking () {
 
     const inputLine = document.querySelector('.js-search-field');
-    inputLine.addEventListener('input', searchFormInputHandler);
+    inputLine.addEventListener('input',
+    // _.debounce(
+        searchFormInputHandler
+        , 500)
+        // );
 }
 
 function searchFormInputHandler (e) { 
@@ -13,7 +26,17 @@ function searchFormInputHandler (e) {
     const value = e.currentTarget.value;
     console.log(value);
     const input = search.getByName(value);
-    input.then(d=>console.log(d));
+    input.then(d=> inputChecking(d));
+
+
 };
+
+function inputChecking(input) {
+    if (typeof input === 'string'){
+        errorString.textContent = input;
+    } else     if (typeof input === 'object'){
+        errorString.textContent = '';
+    }
+}
 eventMaking(); 
 
